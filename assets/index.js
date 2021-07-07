a094c8d21592ec1fee204746aacb0a30ac8fb89c
abdl-dev <me@abdl.dev>
2021-07-07 17:19:41 -0400

    change tabs to spaces

--- assets/index.js
+++ assets/index.js
@@ -1,11 +1,11 @@
 function autoColorScheme() {
-	if (window.matchMedia("(prefers-color-scheme: light)").matches) {
-		document.body.classList.add("light");
-	} else {
-		document.body.classList.add("dark");
-	}
+    if (window.matchMedia("(prefers-color-scheme: light)").matches) {
+        document.body.classList.add("light");
+    } else {
+        document.body.classList.add("dark");
+    }
 }
 
 function setColorScheme(scheme) {
-	document.body.classList.add(scheme);
+    document.body.classList.add(scheme);
 }
